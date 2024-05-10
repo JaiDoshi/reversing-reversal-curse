8160adddcd642a85d517d26955c18446911eaa65
Tanish Bafna <tanishbafna03@gmail.com>
2024-05-09 21:14:03 -0400

    Update evalSys.py

--- src/evalSys.py
+++ src/evalSys.py
@@ -5,6 +5,7 @@ import datetime
 from functools import partial
 from dotenv import load_dotenv; load_dotenv()
 import pandas as pd
+from tqdm import tqdm 
 
 import torch
 from datasets import load_from_disk
@@ -25,30 +26,22 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 #--------------------------------
 
-def generate_output(model, tokenizer, messages, pipe, generate_kwargs):
+def generate_output(messages, pipe, generate_kwargs):
 
-    input_ids = tokenizer.apply_chat_template(
+    input_ids = pipe.tokenizer.apply_chat_template(
         messages,
         add_generation_prompt=True,
         return_tensors="pt"
-    ).to(model.device)
+    ).to(pipe.device)
 
-    outputs = model.generate(
-        input_ids,
-        **generate_kwargs,
-    )
+    outputs = pipe.model.generate(input_ids, **generate_kwargs)
 
-    response = tokenizer.decode(outputs[0][input_ids.shape[-1]:], skip_special_tokens=True)
-    return pipe(response, **generate_kwargs)
+    response = pipe.tokenizer.decode(outputs[0][input_ids.shape[-1]:], skip_special_tokens=True)
+    return response
 
 def main(args):
 
     tokenizer = AutoTokenizer.from_pretrained(args.tokenizer, padding_side="right", token=HUGGINGFACE_TOKEN)
-
-    # Add padding tokens
-    tokenizer.padding_side = "right"
-    tokenizer.pad_token = tokenizer.eos_token
-    tokenizer.pad_token_id = tokenizer.eos_token_id
 
     terminators = [
         tokenizer.eos_token_id,
@@ -60,7 +53,7 @@ def main(args):
     model.to(device)
 
     pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, device=device, return_full_text=False)
-    generate_kwargs = {'max_new_tokens': 30, 'temperature': 0.01, 'eos_token_id': terminators, 'do_sample': False}
+    generate_kwargs = {'max_new_tokens': 30, 'temperature': 0.01, 'eos_token_id': terminators}
 
     files = {
         'd2p_qa_test': 'data/nlu_experiments/validation-test/d2p_qa_test.json',
@@ -76,16 +69,13 @@ def main(args):
 
         data = json.load(open(file))
         for entry in data:
-            messages[1]['content'] = entry['p2d']['question']
-            output = generate_output(
-                model=model, tokenizer=tokenizer,
-                messages=messages, pipe=pipe, generate_kwargs=generate_kwargs
-            )
+            for Qtype in ['p2d', 'd2p']:
+                messages[1]['content'] = entry[Qtype]['question']
+                output = generate_output(
+                    messages=messages, pipe=pipe, generate_kwargs=generate_kwargs
+                )
 
-            entry['p2d']["predicted_answer"] = output[0]['generated_text']
-
-            output = generate_output(entry['d2p']['question'], pipe, generate_kwargs)
-            entry['d2p']["predicted_answer"] = output[0]['generated_text']
+                entry[Qtype]["predicted_answer"] = output[0]['generated_text']
 
         df = pd.DataFrame([
             {
@@ -96,7 +86,7 @@ def main(args):
             for key, value in item.items() if key in ['p2d', 'd2p']
         ])
 
-        df.to_csv(os.path.join(args.experiment_path, f'{fileType}_outputs.csv'), index=False)
+        df.to_csv(os.path.join(args.experiment_path, f'{fileType}_sys_outputs.csv'), index=False)
 
 if __name__ == "__main__":
 
@@ -109,5 +99,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
     main(args)
 
-#python src/evaluate.py --model-dir data/nlu_experiments/Exp1_A_Original/model_full_2024-05-05_10-03 --experiment-path data/nlu_experiments/Exp1_A_Original
-#python src/evaluate.py --model-dir data/nlu_experiments/Exp1_B_Meta_Augment/model_full_2024-05-05_13-35 --experiment-path data/nlu_experiments/Exp1_B_Meta_Augment
+#python src/evalSys.py --model-dir data/nlu_experiments/Exp1_A_Original/model_full_2024-05-05_10-03 --experiment-path data/nlu_experiments/Exp1_A_Original
+#python src/evalSys.py --model-dir data/nlu_experiments/Exp1_B_Meta_Augment/model_full_2024-05-05_13-35 --experiment-path data/nlu_experiments/Exp1_B_Meta_Augment
